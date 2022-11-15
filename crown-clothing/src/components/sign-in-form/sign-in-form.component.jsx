85acdf4a718385ce5f95b990897445f3c7c27fb6
ajit1690 <ajit.singh1690@hotmail.com>
2022-11-16 01:42:13 +0530

    signin and Signup completed

---
+++ crown-clothing/src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+import { createUserDocumentFromAuth, signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import './sign-in-form.styles.scss';
+
+const defaultFormFields = {
+  email: '',
+  password: ''
+}
+
+const SignInFrom = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+
+  const { email, password} = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  }
+  
+  const signInWithGoogle = async () => {
+    const {user} = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(email, password);
+
+
+    } catch(error) {
+      console.log(error);
+    }
+
+  }
+
+  const handleChange = (event) => {
+      const {name, value} = event.target;
+
+      setFormFields({ ...formFields, [name]: value});
+  }; 
+
+  return(
+    <div className="sign-in-container">
+      <h2>Already have an account!</h2>
+      <span>Sign in with Email and Password</span>
+      <form onSubmit={handleSubmit}>
+        
+              
+        <FormInput label='Email' type='email' required onChange={handleChange} name='email' value={email}/>
+
+        
+        <FormInput label="Password" type='password' required onChange={handleChange} name='password' value={password}/>
+
+        <div className="buttons-container">
+          <Button buttonType='' type='submit'>Sign In</Button>
+          <Button type="button" buttonType='google' onClick={signInWithGoogle}>Sign in with Google</Button>
+        </div>  
+
+      </form>
+    </div>
+  )
+}
+
+export default SignInFrom;
